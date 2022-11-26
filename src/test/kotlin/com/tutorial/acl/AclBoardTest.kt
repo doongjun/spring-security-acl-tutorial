d109c3bdc075c2a20567cdfa69d98b031743f9a6
doongjun <doongjun.kim@gmail.com>
2022-11-27 00:46:08 +0900

    feat: 게시판 예제

---
+++ src/test/kotlin/com/tutorial/acl/AclBoardTest.kt
@@ -0,0 +1,31 @@
+package com.tutorial.acl
+
+import com.tutorial.acl.domain.Board
+import com.tutorial.acl.repository.BoardRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.context.support.WithMockUser
+
+@SpringBootTest
+class AclBoardTest {
+
+    @Autowired
+    private lateinit var boardRepository: BoardRepository
+
+    private val noticeId = 1
+
+    @Test
+    @WithMockUser(username = "userA")
+    fun `userA_게시판_전체조회`() {
+        //when
+        val findAll: List<Board> = boardRepository.findAll()
+
+        //then
+        assertThat(findAll.size).isEqualTo(1)
+        assertThat(findAll[0].id).isEqualTo(noticeId)
+        assertThat(findAll[0].name).isEqualTo("공지사항")
+    }
+
+}
