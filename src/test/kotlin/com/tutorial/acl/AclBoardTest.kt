49c3b49712de6062866a1aacbb795f7d59cb9d5f
doongjun <doongjun.kim@gmail.com>
2023-01-30 17:51:19 +0900

    feat: add logging

--- src/test/kotlin/com/tutorial/acl/AclBoardTest.kt
+++ src/test/kotlin/com/tutorial/acl/AclBoardTest.kt
@@ -4,12 +4,14 @@ import com.tutorial.acl.domain.Board
 import com.tutorial.acl.repository.BoardRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.context.support.WithMockUser
 
 @SpringBootTest
 class AclBoardTest {
+    private val log = LoggerFactory.getLogger(javaClass)
 
     @Autowired
     private lateinit var boardRepository: BoardRepository
@@ -21,6 +23,8 @@ class AclBoardTest {
     fun `userA_게시판_전체조회`() {
         //when
         val findAll: List<Board> = boardRepository.findAll()
+        val names = findAll.map { it.name }.joinToString(",")
+        log.info(names)
 
         //then
         assertThat(findAll.size).isEqualTo(1)
